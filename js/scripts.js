eb47283028b67c38529b1aff0c9d5574bc7fd0f9
nifferdil <nifferdil@gmail.com>
2015-09-18 17:24:03 -0700

    pass first spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
+var bottlesofBeerFirst = function(number) {
+    var countDown = number - 1;
+    return number + " bottles of beer on the wall, " + number + " bottles of beer. Take one down and pass it around, " + countDown + " bottles of beer on the wall.";
+};
 
+
+  //  for (var i = number; i <= number; i--) {
+
+$(document).ready(function() {
+  $("form#bottleNumber").submit(function(event) {
+    number = parseInt($("input#number").val());
+
+    var result = bottlesofBeerFirst(number);
+
+    $(".output").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,11 @@
-describe('getNumber', function() {
-  it("returns number entered from a user", function() {
-    expect(getNumber(99).to.equal("99");
+describe('bottlesofBeerFirst', function() {
+  it("counts down one number from the user inputted number", function() {
+    expect(bottlesofBeerFirst(99)).to.equal("99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around, 98 bottles of beer on the wall.");
   });
+});
+
+// describe('bottlesofBeer', function() {
+//   it("is true when the function returns the factorial of the inputted number", function() {
+//     expect(bottlesofBeer(5)).to.equal(120);
+//   });
+// });
